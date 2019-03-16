0de66df9ea8148fd1bb52f8d7e929ac667df4456
Rakinwale731 <48632489+Rakinwale731@users.noreply.github.com>
2019-03-16 16:42:41 -0400

    Add files via upload

---
+++ Led_blank.ino
@@ -0,0 +1,20 @@
+int ledPin=13;
+int PinButton1=2;
+void setup() {
+  pinMode(ledPin,OUTPUT);
+  pinMode(PinButton1,INPUT);
+  digitalWrite(PinButton1,HIGH);
+}
+
+void loop() {
+  digitalWrite(ledPin,HIGH);
+  delay(300);
+  digitalWrite(ledPin,LOW);
+  delay(300);
+  
+//if (digitalRead(PinButton1)==LOW){
+    digitalWrite(ledPin,HIGH);
+   
+}
+//else{
+   // digitalWrite(ledPin,LOW);
